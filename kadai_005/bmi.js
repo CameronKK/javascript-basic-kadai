700557a86b7a9a6c89c86497b285fe7104b8e607
Tomo <ktomotomo58sky@gmail.com>
2024-10-30 13:00:01 +0900

    bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,12 @@
 //変数を定義する
 let userWeight;
 let userHeight;
+let bmi;
 
 //値を代入する
-userWeight = '68';
-userHeight ='1.7';
+userWeight = 68;
+userHeight = 1.7;
+bmi = userWeight/userHeight/userHeight;
 
 //コンソールに出力する
-console.log(userWeight/userHeight/userHeight);
+console.log(bmi);
